7204a9af24035f2a6f4efcd5e374a7dbc3aaa71d
olubiyi95 <82479253+olubiyi95@users.noreply.github.com>
2023-08-10 12:04:13 +0100

    feat: recent changes

--- src/components/Recipescard.tsx
+++ src/components/Recipescard.tsx
@@ -1,5 +1,5 @@
-import React, {FC} from 'react'
-import { Card, CardHeader, CardBody, HStack, Box, Stack, Text,  Heading, useBreakpointValue, Button } from '@chakra-ui/react';
+import React, { FC } from 'react'
+import { Card, CardHeader, CardBody, HStack, Box, Stack, Text, Heading, useBreakpointValue, Button } from '@chakra-ui/react';
 import Image from 'next/image';
 
 
@@ -7,75 +7,75 @@ import Image from 'next/image';
 type Navigate2 = () => void
 
 interface Cardprops {
-  image:any, 
-  title:string, 
-  description:string, 
-  link:Navigate2, 
-  linktext:string
+  image: any,
+  title: string,
+  description: string,
+  link: Navigate2,
+  linktext: string
 }
 
-const Recipescard:FC<Cardprops> = ({image, title, description, link, linktext}) => {
-  
+const Recipescard: FC<Cardprops> = ({ image, title, description, link, linktext }) => {
+
   const cardWidth = useBreakpointValue({ base: '320px', sm: '320px', md: '300px', lg: '100%' });
   const cardHeight = useBreakpointValue({ base: '400px', sm: '400px', md: '400px', lg: '180px' });
-  const imageWidth =  useBreakpointValue({ base: '350px', sm: '350px', md: '350px', lg: '181px' });
+  const imageWidth = useBreakpointValue({ base: '350px', sm: '350px', md: '350px', lg: '181px' });
 
   interface imagestyle {
-    width:any,
-    height:string,
+    width: any,
+    height: string,
     //borderRadius:string,
-    backgroundPosition:string
- }
+    backgroundPosition: string
+  }
 
-const imageStyle:imagestyle = {
+  const imageStyle: imagestyle = {
     width: imageWidth,
-     height:'100%',
+    height: '100%',
     //borderRadius:'5.5%  0 0 5.5%',
-    backgroundPosition:'cover'
+    backgroundPosition: 'cover'
   }
 
   interface Linkstyle {
-    fontSize:string,
-    color:string
+    fontSize: string,
+    color: string
   }
 
-  const linkStyle:Linkstyle = {
-    fontSize:'18px',
-    color:'#fe9500'
+  const linkStyle: Linkstyle = {
+    fontSize: '18px',
+    color: '#fe9500'
   }
-  
+
   return (
-   <Box>
-    <Card
-  direction={{ base: 'column', sm: 'column', lg:'row' }} align={'flex-start'}
-  boxShadow='md' rounded='md' bg='white' w={cardWidth} h={cardHeight}  zIndex='1' borderRadius={'20px'}  _hover={{ transition: 'all 0.3s ease',transform: 'scale(1.030)'}} overflow={'hidden'}
->
-  <Image
-    src={image}
-    alt='Caffe Latte'
-    style={imageStyle}
-    object-fit="cover"
-    
-  />
+    <Box>
+      <Card
+        direction={{ base: 'column', sm: 'column', lg: 'row' }} align={'flex-start'}
+        boxShadow='md' rounded='md' bg='white' w={cardWidth} h={cardHeight} zIndex='1' borderRadius={'20px'} _hover={{ transition: 'all 0.3s ease', transform: 'scale(1.030)' }} overflow={'hidden'}
+      >
+        <Image
+          src={image}
+          alt='Caffe Latte'
+          style={imageStyle}
+          object-fit="cover"
 
-  <Stack >
-    <CardBody >
-      <Heading size='md' align='left' >{title}</Heading>
-      <Text py='2' align={'left'}>{description}</Text>
-      <HStack align={'flex-start'}>
-      <Button onClick={link} className='recipecardbutton' size={'md'} textAlign='left' px={0} _hover={{backgroundColor:'initual'}}>
-        <Text align='left'>
-        {linktext}
-        </Text>
-      </Button>
-      </HStack>
-    
-      {/* <Text style={linkStyle} onClick={link} align={'left'}>{linktext}</Text> */}
-    </CardBody>
-    
-  </Stack>
-</Card>
-   </Box>
+        />
+
+        <Stack >
+          <CardBody >
+            <Heading size='md' align='left' >{title}</Heading>
+            <Text py='2' align={'left'}>{description}</Text>
+            <HStack align={'flex-start'}>
+              <Button onClick={link} className='recipecardbutton' size={'md'} textAlign='left' px={0} _hover={{ backgroundColor: 'initual' }}>
+                <Text align='left'>
+                  {linktext}
+                </Text>
+              </Button>
+            </HStack>
+
+            {/* <Text style={linkStyle} onClick={link} align={'left'}>{linktext}</Text> */}
+          </CardBody>
+
+        </Stack>
+      </Card>
+    </Box>
   )
 }
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,17 @@
-import React, { FC } from 'react'
+import React, { FC } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import 'antd/dist/reset.css';
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { Roboto } from 'next/font/google';
-import '../styles/global.scss'
-import { extendTheme } from '@chakra-ui/react'
+import '../styles/global.scss';
+import { extendTheme } from '@chakra-ui/react';
 import styled from "@emotion/styled";
 import { Rubik } from 'next/font/google';
-import '@fontsource/raleway/400.css'
-import '@fontsource/open-sans/700.css'
+import '@fontsource/raleway/400.css';
+import '@fontsource/open-sans/700.css';
+import NextNProgress from 'nextjs-progressbar';
 
 
 const roboto = Roboto({
@@ -40,6 +41,7 @@ const _app: FC<AppProps> = ({ Component, pageProps }) => {
         }
       `}
       </style> */}
+      <NextNProgress color='#fe9500' />
       <ChakraProvider toastOptions={{ defaultOptions: { position: 'top' } }} theme={theme}>
         <Component {...pageProps} />
       </ChakraProvider>
